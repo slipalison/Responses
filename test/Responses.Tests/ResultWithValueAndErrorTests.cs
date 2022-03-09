162a303c85f59a12a0eb81718e332ae1fda3b34e
Alison Amorim <alisonmsn@hotmail.com>
2022-03-09 16:42:09 -0300

    Update ResultWithValueAndErrorTests.cs

--- test/Responses.Tests/ResultWithValueAndErrorTests.cs
+++ test/Responses.Tests/ResultWithValueAndErrorTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Flurl.Http;
 using Xunit;
 using static Responses.Tests.TestUtils;
@@ -16,8 +16,10 @@ namespace Responses.Tests
         [InlineData(299)]
         public async Task Ok(int status)
         {
-            var result = await FakeRequest(status, true)
-                .ReceiveResult<bool, Error>();
+            var result = await FakeRequest(status, true).ReceiveResult<bool, Error>();
+            var result2 = await FakeRequestFlurl(status, true).ReceiveResult<bool, Error>();
+            Assert.True(result2.IsSuccess);
+            Assert.True(result2.Value);
 
             Assert.True(result.IsSuccess);
             Assert.True(result.Value);
@@ -38,11 +40,22 @@ namespace Responses.Tests
             var result = await FakeRequest(status, ErrorJson(status.ToString(), "ANY ERROR", "Core", "ANY1"))
                 .ReceiveResult<bool, Error>();
 
+            var result2 = await FakeRequestFlurl(status, ErrorJson(status.ToString(), "ANY ERROR", "Core", "ANY1"))
+                .ReceiveResult<bool, Error>();
+
+
+
             Assert.False(result.IsSuccess);
             Assert.Equal("Core", result.Error.Layer);
             Assert.Equal(status.ToString(), result.Error.Code);
             Assert.Equal("ANY ERROR", result.Error.Message);
             Assert.Equal("ANY1", result.Error.ApplicationName);
+
+            Assert.False(result2.IsSuccess);
+            Assert.Equal("Core", result2.Error.Layer);
+            Assert.Equal(status.ToString(), result2.Error.Code);
+            Assert.Equal("ANY ERROR", result2.Error.Message);
+            Assert.Equal("ANY1", result2.Error.ApplicationName);
         }
     }
 }
